e59cc069e5b99d8b6f5bdf3816478b81f8f8688f
Abdi-01 <abdialghi@gmail.com>
2025-07-01 10:02:24 +0700

    create components dialog

---
+++ src/app/my-article/components/UpdateArticleDialog.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface IUpdateArticleDialogProps {}
+
+const UpdateArticleDialog: React.FunctionComponent<
+  IUpdateArticleDialogProps
+> = (props) => {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="sm">
+            Edit
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+};
+
+export default UpdateArticleDialog;
